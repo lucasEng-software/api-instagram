a9d1a79f953e560806b56d35c5d953e254a12030
lucasEng-software <lucascaefeitosa@alu.ufc.br>
2023-08-24 16:53:50 -0300

    atualização das rotas, inclusão do schemavalidator e controller

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
+
+const UserController = require('./apps/controllers/UserController')
 const UserModel = require('./apps/models/User');
 
+const schemaValidator = require('./apps/middlewares/schemavalidator');
+const userSchema = require('./schema/create.user.schema.json')
+
 const routes = new Router();
+
 routes.get('/health', (req, res) => res.send({ message: 'Conectado com sucsso!' }));
 
+routes.get('/user', UserController.list);
+routes.post('/user', schemaValidator(userSchema), UserController.create);
 
-routes.get('/users', async (req, res) => {
-    const allUsers = await UserModel.findAll();
-    res.send({users:allUsers});
-});
 module.exports = routes;
