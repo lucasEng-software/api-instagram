c921053c7c67107fd21ac6c9a18947d27838a45d
lucasEng-software <lucascaefeitosa@alu.ufc.br>
2023-09-06 14:42:05 -0300

    Add cors

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
 require('dotenv').config();
 require('./database/index')
 
+const cors = require('cors');
 const express = require('express');
 const routes = require('./routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-const {PORT} = process.env;
 
+const {PORT} = process.env;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
